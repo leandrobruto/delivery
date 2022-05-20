38b70d839ab671cbee8dab880e8ef1e56cd088d0
Leandro <leandrobruto42@gmail.com>
2022-05-20 17:01:36 -0300

    fix descomentando controller seed

--- app/Controllers/Seed.php
+++ app/Controllers/Seed.php
@@ -8,11 +8,12 @@ class Seed extends BaseController
 {
     public function index()
     {
-    //     $seeder = \Config\Database::seeder();
+        $seeder = \Config\Database::seeder();
 
-    //     $seeder->call('UsuarioSeeder');
-    //     $seeder->call('ExpedienteSeeder');
-    //     $seeder->call('FormasSeeder');
-    echo 'Semeado.';
+        $seeder->call('UsuarioSeeder');
+        $seeder->call('ExpedienteSeeder');
+        $seeder->call('FormasSeeder');
+        
+        echo 'Semeado.';
     }
 }
